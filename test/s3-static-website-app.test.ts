977065cf20c2e5962624be54736b793f9e93160d
Ryan Token <ryantoken13@icloud.com>
2021-04-17 17:13:24 -0500

    Initial commit

---
+++ test/s3-static-website-app.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as S3StaticWebsiteApp from '../lib/s3-static-website-app-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new S3StaticWebsiteApp.S3StaticWebsiteAppStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
